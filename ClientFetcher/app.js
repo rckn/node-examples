92b66646cb289faf48ec38ac421a3b877f36e6b9
Rasmus Christiansen Knap <rckn@ucn.dk>
2017-03-30 11:22:59 +0200

    Got the json stream example to work

--- ClientFetcher/app.js
+++ ClientFetcher/app.js
@@ -1,22 +1,14 @@
-var clientfetchter = require('./clientFetcher');
+const clientfetchter = require('./clientFetcher');
 
-var client = clientfetchter.getClient(1, (err, result) => {
+// var client = clientfetchter.getClient(1, (err, result) => {
+//     if(err) console.log(err);
+
+//     console.log(result);
+// });
+
+
+clientfetchter.getStreamClient(1, '*', (err, data) => {
     if(err) console.log(err);
 
-    console.log(result);
+    console.log(data);
 });
-
-/*
-clientfetchter.getStreamClient(1, (err, data) => {
-     if(err) console.log(err);
-
-    console.log(result);
-});
-
-
-
-// console.log(client);
-
-// clientfetchter.getClient
-
-console.log('done');
--- ClientFetcher/clientFetcher.js
+++ ClientFetcher/clientFetcher.js
@@ -1,10 +1,9 @@
 var fs = require('fs');
 var JSONStream = require('JSONStream');
-var es = require('event-stream');
 
 var getClientSync = (id) => {
-    
-    var file = fs.readFileSync(__dirname + `/Clients/${id}.json`,'utf8');
+
+    var file = fs.readFileSync(__dirname + `/Clients/${id}.json`, 'utf8');
 
     var client = JSON.parse(file);
 
@@ -13,27 +12,43 @@ var getClientSync = (id) => {
 
 module.exports.getClientSync = getClientSync;
 
-var getClient = function(id, cb) {
+var getClient = function (id, cb) {
 
-    fs.readFile(__dirname + `/Clients/${id}.json`,'utf8',(err, data) => {
-            if(err) return cb(err);
-            var client = JSON.parse(data);
-            cb(null, data);
+    fs.readFile(__dirname + `/Clients/${id}.json`, 'utf8', (err, data) => {
+        if (err) return cb(err);
+        var client = JSON.parse(data);
+        cb(null, data);
     });
 }
 
 module.exports.getClient = getClient;
 
-
-// This is a work in progress does not work yet!
-var getStreamClient = (id, cb) => {
-    var readStream = fs.createReadStream(__dirname + `/Clients/${id}.json`);
-    var parser = JSONStream.parse('.Name');
-    readStream
-    .pipe(parser)
-    .pipe(es.map(data => {
-        cb(null,data);
-    }));
+// The purpose of the stream client is to fecth json data
+// without buffering an entire json document, if it is not needed
+// Through the searchParameter one can narrow down the json returned from the stream
+var getStreamClient = (id, searchParameter, cb) => {
+    try {
+        // if no searchParameter provided, we return the entire json document '*'
+        var searchParameter = searchParameter || '*';
+        // waterMark -> our buffersize in number of bytes for each chunk. 1024 bytes = 1 kb. 
+        var readStream = fs.createReadStream(__dirname +
+            `/Clients/${id}.json`, { highWaterMark: 16 * 1024 });
+        var parser = JSONStream.parse(searchParameter);
+        readStream
+            .pipe(parser)
+            .on('data', (data) => {
+                // some dots to show each buffered object.
+                // you will see that entire nested objects and arrays will get buffered (a bit of a gotcha)!
+                console.log('.........................................................')
+                cb(null, data);
+            });
+    } catch (error) {
+        // wrap our callback into a setImmediate
+        // we do this to make our callback is always 
+        // called asynchronoulsy. 
+        // Which make our clientFetcher api consistent
+        setImmediate(cb(error));
+    }
 }
 
 module.exports.getStreamClient = getStreamClient;
